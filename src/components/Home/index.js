4f276f513ee8136a7c02a7722253649289455d4b
chaitanyakumard <130781424+chaitanyakumard@users.noreply.github.com>
2024-01-26 10:03:15 +0530

    Update index.js
    add to added buttonn functionality

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./styles.css"
+import "./styles.css";
 
-
-const Home = ({addProject}) => {
+const Home = ({ addProject }) => {
   const [projectName, setProjectName] = useState('');
   const [projectLink, setProjectLink] = useState('');
   const [projectDescription, setProjectDescription] = useState('');
@@ -33,11 +32,13 @@ const Home = ({addProject}) => {
       link: projectLink,
       description: projectDescription,
     };
-    addProject(newProject);
     // Clear input fields
     setProjectName('');
     setProjectLink('');
     setProjectDescription('');
+    addProject(newProject);
+    
+    
   };
 
   const handleNavigateToProjects = () => {
@@ -47,48 +48,50 @@ const Home = ({addProject}) => {
   return (
     <div className='home-container'>
       <div className='first-page-bg'>
-      <div className='details-page'>
-      <p className='home-paragraph'>UI/UX DESIGNER</p>
-      <h2>Hello, my name<br/>is Modelyn Torff</h2>
-      <p className='home-description'>Short text with details about you, what you do or your professional career. You can add more information on the about page.</p>
-      <div>
-        <button type='button' className='project-button' onClick={handleNavigateToProjects}>Projects</button>
-        <button>Linkedin</button>
+        <div className='details-page'>
+          <p className='home-paragraph'>UI/UX DESIGNER</p>
+          <h2>Hello, my name<br />is Modelyn Torff</h2>
+          <p className='home-description'>Short text with details about you, what you do or your professional career. You can add more information on the about page.</p>
+          <div>
+            <button type='button' className='project-button' onClick={handleNavigateToProjects}>Projects</button>
+            <button>Linkedin</button>
+          </div>
+        </div>
+        <img className='first-image' src={'./images/Fimage.png'} alt="name" />
       </div>
-      </div>
-      <img className='first-image' src={'./images/Fimage.png'}  alt="name"/>
-    </div>
 
       <div className='project-input-section'>
-      <h3>Add Project</h3>
-        <form  onSubmit={handleAddProject}>
-        <label>Project Name:</label>
-        <input
+        <h3>Add Project</h3>
+        <form onSubmit={handleAddProject}>
+          <label>Project Name:</label>
+          <input
             type="text"
             name="projectName"
+            value={projectName}
             onChange={handleInputChange}
           />
-        <label>Project Link:</label>
-        <input
+          <label>Project Link:</label>
+          <input
             type="text"
             name="projectLink"
+            value={projectLink}
             onChange={handleInputChange}
           />
-        <label>Project Description:</label>
-        <input
+          <label>Project Description:</label>
+          <input
             type="text"
             style={{ height: '150px' }}
             name="projectDescription"
+            value={projectDescription}
             onChange={handleInputChange}
           />
-        <div className='button-container'>
-        <button className='project-button add-button' type="submit">Add</button>
-        </div>
-      </form>
+          <div className='button-container'>
+            <button className='project-button add-button' type="submit">Add</button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
 export default Home;
-
