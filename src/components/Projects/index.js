db7f1c513d6170f476278762ad9d86683b61339b
chaitanyakumard <130781424+chaitanyakumard@users.noreply.github.com>
2024-01-26 10:04:26 +0530

    Update index.js
    added to add button functionality

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import "./styles.css"
+import "./styles.css";
 import Contact from '../Contact';
 
-
-const Projects = ({ projects }) => {
+const Projects = ({ projects, addProject }) => {
   return (
     <div className='projects-bg-container'>
       <div className='project-container'>
         <h3 className='projects-title'>Projects</h3>
-        {projects.map((project, index) => (
+
+        {/* Render existing projects */}
+        {projects.filter(project => !project.isUserAdded).map((project, index) => (
           <div key={index} className={`project-section ${index % 2 === 0 ? 'even-project' : 'odd-project'}`}>
             <div className='description-container'>
-            <h4 className='pro-name'>{project.name}</h4>
-            <p className='pro-description'>{project.description}</p>
-            <button className='view-project-button' type='button'>View Project</button>
-            </div> 
-            <img className='pro-image' src={project.link} alt={`Project ${index + 1}`}/>
+              <h4 className='pro-name'>{project.name}</h4>
+              <p className='pro-description'>{project.description}</p>
+              <button className='view-project-button' type='button'>View Project</button>
+            </div>
+            <img className='pro-image' src={project.link} alt={`Project ${index + 1}`} />
+          </div>
+        ))}
+
+        {/* Render user-added projects */}
+        {projects.filter(project => project.isUserAdded).map((project, index) => (
+          <div key={index} className={`project-section ${index % 2 === 0 ? 'even-project' : 'odd-project'}`}>
+            <div className='description-container'>
+              <h4 className='pro-name'>{project.name}</h4>
+              <p className='pro-description'>{project.description}</p>
+              <button className='view-project-button' type='button'>View Project</button>
+            </div>
+            <img className='pro-image' src={project.link} alt={`Project ${index + 1}`} />
           </div>
         ))}
       </div>
-      <Contact/>
+      <Contact />
     </div>
   );
 }
